353774deb350b24593dd2fa865ce230a2c329c84
mxrtinax <74412168+mxrtinax@users.noreply.github.com>
2022-01-10 17:33:07 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
   if (isset($_SESSION["email"])) {
+      if($_SESSION["isAdmin"]==1){
     include_once 'header.php';
     include_once 'adminPanel.php';}
+      else {header('location: ../e404.php');}
+  }
     else {header('location: ../e404.php');}
-  //echo '<h1>--vtuv'.$_SESSION["email"].'</h1>';
  ?>
